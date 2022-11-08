716c2d6d6450c189dae816c1e0c2935538dd6af4
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2022-11-08 07:33:54 -0500

    221108
    ./www/log/221108.wik
    ACTSVG
    Add Pins template
    
    ./adm/term/221108.wik
    INSTALL PYTHON POETRY
    curl -sSL https://install.python-poetry.org | python3 -
    poetry --version  # see /k/adm/.profile
    Poetry (version 1.2.2)
    
    INSTALL LXML FOR PYTHON 3 PROJECT
    sudo apt-get install libxml2-dev libxslt-dev python3-dev
    # Note, selecting 'libxslt1-dev' instead of 'libxslt-dev'
    # You might want to run 'apt --fix-broken install' to correct these.
    # The following packages have unmet dependencies:
    #  libeval-closure-perl : Depends: libsub-exporter-pmrl but it is not installable
    #  libxml2-dev : Depends: libicu-dev but it is not going to be installed
    #  python3-dev : Depends: python3 (= 3.10.6-1~22.04) but 3.10.4-0ubuntu2 is to be installed
    #                Depends: libpython3-dev (= 3.10.6-1~22.04) but 3.10.4-0ubuntu2 is to be installed
    #                Depends: python3-distutils (>= 3.10.6-1~) but 3.10.4-0ubuntu1 is to be installed
    # E: Unmet dependencies. Try 'apt --fix-broken install' with no packages (or specify a solution).
    python3 --version
    # Python 3.10.6
    sudo apt-get update && sudo apt-get install python3
    # You might want to run 'apt --fix-broken install' to correct these.
    # The following packages have unmet dependencies:
    #  libeval-closure-perl : Depends: libsub-exporter-pmrl but it is not installable
    #  python3 : PreDepends: python3-minimal (= 3.10.6-1~22.04) but 3.10.4-0ubuntu2 is to be installed
    #            Depends: libpython3-stdlib (= 3.10.6-1~22.04) but 3.10.4-0ubuntu2 is to be installed
    # E: Unmet dependencies. Try 'apt --fix-broken install' with no packages (or specify a solution).
    
    sudo apt --fix-broken install
    # Y
    sudo apt-get update && sudo apt-get install python3
    # Y
    python3 --version
    # Python 3.10.6
    sudo apt-get install libxml2-dev libxslt-dev python3-dev
    # Y
    # (success)
    apt-cache show libxml2
    # Package: libxml2
    # Architecture: amd64
    # Version: 2.9.13+dfsg-1ubuntu0.1
    # ..
    
    sudo apt-get install python3-lxml
    python3
    >>> from lxml import etree
    >>> etree.LXML_VERSION
    # (4, 8, 0, 0)
    
    poetry install
    # Warning: poetry.lock is not consistent with pyproject.toml. You may be getting improper dependencies. Run `poetry lock [--no-update]` to fix it.
    poetry lock --no-update
    poetry install
    
    ./adm/log/221108.wik
    POETRY SET-UP
    /home/$USER/.local/bin (see /k/adm/.profile)
    
    PULL CODE
    add MuseReel repo (https://github.com/koreyhinton/MuseReel) in
    /adm/bin/pull

--- www/svg/ActSvgTemplate.js
+++ www/svg/ActSvgTemplate.js
@@ -1,4 +1,4 @@
-swim3ElXml = `
+templateXml = {'swim3': `
 
     <text x="81" y="34" fill="black">Users</text>
     <text x="312" y="34" fill="black">Web Service</text>
@@ -8,16 +8,42 @@ swim3ElXml = `
     <rect rx="0" ry="0" x="246" y="3" width="240" height="745" stroke="black" fill="transparent" stroke-width="1"/>
     <rect rx="0" ry="0" x="3" y="3" width="240" height="745" stroke="black" fill="transparent" stroke-width="1"/>
 
-`;
+`,'pins': `
+
+        <text x="129" y="134" fill="black">Get User</text>
+    <text x="129" y="154" fill="black">Input</text>
+    <text x="222" y="111" fill="black">type</text>
+    <text x="222" y="181" fill="black">input</text>
+    <text x="328" y="123" fill="black">Format</text>
+    <text x="327" y="142" fill="black">Input</text>
+    <text x="545" y="124" fill="black">Format</text>
+    <text x="544" y="142" fill="black">Output</text>
+    <circle cx="50" cy="139" r="10" fill="black" stroke="black" stroke-width="1"/>
+    <rect rx="10" ry="10" x="118" y="115" width="100" height="50" stroke="black" fill="transparent" stroke-width="1"/>
+    <polyline points="65 139 114 139 104 129 114 139 104 149" stroke="black" fill="transparent" stroke-width="1"/>
+    <rect rx="0" ry="0" x="223" y="120" width="15" height="15" stroke="black" fill="transparent" stroke-width="1"/>
+    <rect rx="0" ry="0" x="223" y="145" width="15" height="15" stroke="black" fill="transparent" stroke-width="1"/>
+    <polyline points="245 128 311 128 301 118 311 128 301 138" stroke="black" fill="transparent" stroke-width="1"/>
+    <polyline points="249 155 311 155 301 145 311 155 301 165" stroke="black" fill="transparent" stroke-width="1"/>
+    <rect rx="10" ry="10" x="321" y="104" width="95" height="57" stroke="black" fill="transparent" stroke-width="1"/>
+    <polyline points="423 134 531 134 521 124 531 134 521 144" stroke="black" fill="transparent" stroke-width="1"/>
+    <rect rx="0" ry="0" x="459" y="112" width="15" height="15" stroke="black" fill="transparent" stroke-width="1"/>
+    <rect rx="10" ry="10" x="539" y="103" width="95" height="55" stroke="black" fill="transparent" stroke-width="1"/>
+    <polyline points="641 132 677 132 667 122 677 132 667 142" stroke="black" fill="transparent" stroke-width="1"/>
+    <circle cx="696" cy="131" r="6" fill="black" stroke="black" stroke-width="1"/>
+    <circle cx="696" cy="131" r="10" fill="transparent" stroke="black" stroke-width="1"/>
+
+`
+};
 addEventListener('DOMContentLoaded', (e) => {
     var template = new URL(location.href).searchParams.get("template");
     if (template == null) {
         return;
     }
-    if (template == "swim3") {
+    if ("swim3|pins".indexOf(template) > -1) {
         setTimeout(function() {
             document.getElementById("svgFullTextarea").value = 
-                svgHead + swim3ElXml + svgTrail;
+                svgHead + templateXml[template] + svgTrail;
         }, 400);
     }
 });
